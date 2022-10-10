17b8b270813bbc76e921fa05f4af1f8029bbddd0
dadiyusuf78 <dadiyusuf78@student.esaunggul.ac.id>
2022-10-10 11:04:32 +0700

    pertemuan4

--- tipedata/src.py
+++ tipedata/src.py
@@ -1,19 +1,47 @@
+# String
 a = "hello"
 print (a)
 print (type(a))
-
+#Int
 b = 15
 print (b)
 print (type(b))
-
+# Float
 c = 10.5
 print (c)
 print (type(c))
-
+# Tuple
 d = 20,5
 print (d)
 print(type(d))
-
+# Complex
 e = 1j
 print (e)
 print(type(e))
+z = complex (5-9j)
+print (z)
+# List
+f = ["a","b","c"]
+print (f)
+print (type(f))
+# Set
+g ={"a","b","c"}
+print (g)
+print (type(g))
+# Frozenset
+h = frozenset({1,2,3})
+print(h)
+print(type(h))
+#Boolean
+i = True
+j = False
+print(i,j)
+print(type(i))
+print(type(j))
+# Contoh
+list_f = [1,2,3]
+print (list_f[0])
+tuple_f = 1,2,3
+print (tuple_f[0])
+set_f = {1,2,3}
+print (set_f)
