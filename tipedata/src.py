d0ccffcf19c83593de6f4c764979a9aed79d9a4e
dadiyusuf78 <dadiyusuf78@student.esaunggul.ac.id>
2022-10-10 10:45:05 +0700

    pemrograman4

---
+++ tipedata/src.py
@@ -0,0 +1,19 @@
+a = "hello"
+print (a)
+print (type(a))
+
+b = 15
+print (b)
+print (type(b))
+
+c = 10.5
+print (c)
+print (type(c))
+
+d = 20,5
+print (d)
+print(type(d))
+
+e = 1j
+print (e)
+print(type(e))
